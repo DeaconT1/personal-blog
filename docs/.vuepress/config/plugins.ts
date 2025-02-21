f6f91c8f26f49affedeab7e2975c375ad3bee726
UBC Student <htao02@students.cs.ubc.ca>
2025-02-20 18:14:35 -0800

    remove2D

--- docs/.vuepress/config/plugins.ts
+++ docs/.vuepress/config/plugins.ts
@@ -84,31 +84,31 @@ export default <UserPlugins>[
     }],
 
 
-    [
-        'vuepress-plugin-helper-live2d', {
-        // 是否开启控制台日志打印(default: false)
-        log: false,
-        live2d: {
-            // 是否启用(关闭请设置为false)(default: true)
-            enable: true,
-            // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
-            model: 'wanko',
-            display: {
-                position: "left", // 显示位置：left/right(default: 'right')
-                width: 189, // 模型的长度(default: 135)
-                height: 420, // 模型的高度(default: 300)
-                hOffset: 13, //  水平偏移(default: 65)
-                vOffset: -91, //  垂直偏移(default: 0)
-            },
-            mobile: {
-                show: false // 是否在移动设备上显示(default: false)
-            },
-            react: {
-                opacity: 0.8 // 模型透明度(default: 0.8)
-            }
-        }
-    }
-    ],
+    // [
+    //     'vuepress-plugin-helper-live2d', {
+    //     // 是否开启控制台日志打印(default: false)
+    //     log: false,
+    //     live2d: {
+    //         // 是否启用(关闭请设置为false)(default: true)
+    //         enable: true,
+    //         // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+    //         model: 'wanko',
+    //         display: {
+    //             position: "left", // 显示位置：left/right(default: 'right')
+    //             width: 189, // 模型的长度(default: 135)
+    //             height: 420, // 模型的高度(default: 300)
+    //             hOffset: 13, //  水平偏移(default: 65)
+    //             vOffset: -91, //  垂直偏移(default: 0)
+    //         },
+    //         mobile: {
+    //             show: false // 是否在移动设备上显示(default: false)
+    //         },
+    //         react: {
+    //             opacity: 0.8 // 模型透明度(default: 0.8)
+    //         }
+    //     }
+    // }
+    // ],
 
     ["sakura", {
         num: 20,  // 默认数量
