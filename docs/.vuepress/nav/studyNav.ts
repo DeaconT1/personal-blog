1813f71dc5c2ac3c7df6f150e9aaacf09aad38e2
UBC Student <htao02@students.cs.ubc.ca>
2025-02-28 00:31:13 -0800

    新增板块：算法/个人学习，日常文章添加

--- docs/.vuepress/nav/studyNav.ts
+++ docs/.vuepress/nav/studyNav.ts
@@ -1,5 +1,7 @@
 export default {
     text: '学习', link: '/study/', items: [
         {text: 'UBC', link: '/study/UBC/'},
+        {text: '算法', link: '/study/algorithm/'},
+        {text: '个人规划', link: '/study/plan/'},
     ],
 }
