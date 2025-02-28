5007b7fa7bb2d97c52bc0ac693039365a6ace7d8
UBC Student <htao02@students.cs.ubc.ca>
2025-02-28 11:52:22 -0800

    twikoo评论区成功部署～

--- docs/.vuepress/config/head.ts
+++ docs/.vuepress/config/head.ts
@@ -5,7 +5,7 @@ export default <HeadTags> [
     ['link', { rel: 'icon', href: 'https://s3.bmp.ovh/imgs/2025/02/17/90894ee9bae75aac.jpg' }],
     ['link', { rel: 'stylesheet', href: 'https://at.alicdn.com/t/font_3077305_pt8umhrn4k9.css' }],
     ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
-    ['script', { src: 'https://cdn.staticfile.org/twikoo/1.6.7/twikoo.all.min.js' }],
+    ['script', { src: 'https://cdn.staticfile.org/twikoo/1.6.41/twikoo.all.min.js' }],
 
 
 ]
